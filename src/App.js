2040000090df81b93454ef928fd9893b7a6d0b40
Iqbal Alhuda <iqbalalhuda1998@gmail.com>
2022-11-20 11:47:17 +0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Halo React App</h1>
+      <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
